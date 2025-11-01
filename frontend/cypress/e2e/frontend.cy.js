d2daa0bb0f259c947c53db6a51dcfd1e8d5895b9
Laura-702 <Villaraza.Laura@student.greenriver.edu>
2025-10-31 20:49:55 -0700

    Added more cypress tests for frontend.cy.js

--- frontend/cypress/e2e/frontend.cy.js
+++ frontend/cypress/e2e/frontend.cy.js
@@ -1,8 +1,24 @@
-describe('Frontend App', () => {
-    it('renders the greeting and button', () => {
-      cy.visit('/');
-      cy.contains('Hello, Cypress Tester!').should('be.visible');
-      cy.contains('Click me').click();
-    });
+describe('Greeting Machine', () => {
+  beforeEach(() => {
+    cy.visit('/');
   });
-  
+
+  it('shows the greeting machine title', () => {
+    cy.contains('Greeting Machine').should('be.visible');
+  });
+
+  it('displays the first greeting', () => {
+    cy.contains('Hello, Cypress Tester!').should('be.visible');
+  });
+
+  it('changes quote when clicking "Next Quote"', () => {
+    cy.contains('Next Quote').click();
+    cy.contains('Hello, Cypress Tester!').should('not.exist');
+  });
+
+  it('resets to the first quote when clicking "Reset"', () => {
+    cy.contains('Next Quote').click();
+    cy.contains('Reset').click();
+    cy.contains('Hello, Cypress Tester!').should('be.visible');
+  });
+});
